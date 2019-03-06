86aacaf5adeaf312a0b9240d0fccf417e0474ddb
jiahuan <jiahuanye123@163.com>
2019-03-06 17:38:39 -0600

    add if num_each[i]>1
    skip segment with only one number

--- algorithm2.R
+++ algorithm2.R
@@ -6,106 +6,156 @@ X=matrix(c(a,b,c))
 N=dim(X)[1];D=dim(X)[2]
 K=5
 if (N <= K) {
-  warning("Input dimension error!")
+    warning("Input dimension error!")
 } 
 if (K==1) {
-  changePoints = N
-  wgss=var(x)*(N-1)
+    changePoints = N
+    wgss=var(x)*(N-1)
 } else {
-  num_each=matrix(0,nrow=K,ncol=1)
-  wgss_each=matrix(0,nrow=K,ncol=D)
-  mean_each=matrix(0,nrow=K,ncol=D)
-  changePoints=floor(1+(N-2)*runif(K-1))
-  changePoints=c(changePoints,N)
-  changePoints=unique(changePoints)
-  
-  while (length(changePoints)<K) {
-    changePoints=c(changePoints,floor(1+(N-2)*runif(1)))
+    num_each=matrix(0,nrow=K,ncol=1)
+    wgss_each=matrix(0,nrow=K,ncol=D)
+    mean_each=matrix(0,nrow=K,ncol=D)
+    changePoints=floor(1+(N-2)*runif(K-1))
+    changePoints=c(changePoints,N)
     changePoints=unique(changePoints)
-  }
-  
-  changePoints = sort(changePoints)
-  
-  num_each[1] = changePoints[1] 
-  wgss_each[1,] = apply(matrix(X[1:changePoints[1],]),2,var) * (num_each[1]-1)
-  mean_each[1,] = apply(matrix(X[1:changePoints[1],]),2,mean)
-  
-  for (i in 2:K) {
-  num_each[i] = changePoints[i] - changePoints[i-1]
-  # segment 4 only has one number with var NA
-  wgss_each[i,] = apply(matrix(X[(changePoints[i-1]+1):changePoints[i],]),2,var) * (num_each[i]-1)
-  mean_each[i,] = apply(matrix(X[(changePoints[i-1]+1):changePoints[i],]),2,mean) 
-  }
-  
-  iter=0; move=1; maxIter = N
-  while ((move==1) & (iter < maxIter)) {
-    move=0
-    iter=iter+1
-    # test
-    #i=1
-    #test
-    for (i in 1:K-1) {
-      best_gain_sum=-Inf
-      for (ell in 1:num_each[i]-1) { 
+    
+    while (length(changePoints)<K) {
+        changePoints=c(changePoints,floor(1+(N-2)*runif(1)))
+        changePoints=unique(changePoints)
+    }
+    
+    changePoints = sort(changePoints)
+    
+    num_each[1] = changePoints[1] 
+    wgss_each[1,] = apply(matrix(X[1:changePoints[1],]),2,var) * (num_each[1]-1)
+    mean_each[1,] = apply(matrix(X[1:changePoints[1],]),2,mean)
+    
+    for (i in 2:K) {
+        num_each[i] = changePoints[i] - changePoints[i-1]
+        # segment 1 only has one number with var NA
+        wgss_each[i,] = apply(matrix(X[(changePoints[i-1]+1):changePoints[i],]),2,var) * (num_each[i]-1)
+        mean_each[i,] = apply(matrix(X[(changePoints[i-1]+1):changePoints[i],]),2,mean) 
+    }
+    
+    iter=0; move=1; maxIter = N
+    while ((move==1) & (iter < maxIter)) {
+        move=0
+        iter=iter+1
+        # test
+        #i=1;
         #test
-        #ell=2
-        #test
-        mean_candidatePart=apply(matrix(X[(changePoints[i]-ell+1):changePoints[i],]),2,mean)
-      
-        decrease=ell*num_each[i]/(num_each[i]-ell)*(mean_each[i,]-mean_candidatePart)^2 
-        increase=ell*num_each[i+1]/(num_each[i+1]+ell)*(mean_each[i+1,]-mean_candidatePart)^2 
-        if ( sum(decrease) - sum(increase) > best_gain_sum) { 
-          best_gain = decrease - increase
-          best_gain_sum = sum(best_gain)
-          best_ell = ell
-          best_candidatePart = matrix(X[(changePoints[i]-ell+1):changePoints[i],]) 
-          best_decrease = decrease
-          best_increase = increase
+        for (i in 1:(K-1)) {
+            #test 
+            cat("i=",i,"\n")
+            cat("num_each=",num_each,"\n")
+            cat("num_each[i]=",num_each[i],"\n")
+            #test
+            best_gain_sum=-Inf
+            if (num_each[i]>1) {
+                
+                for (ell in 1:(num_each[i]-1)) { 
+                    #test
+                    cat("ell=",ell,"\n")
+                    #test
+                    
+                    mean_candidatePart=apply(matrix(X[(changePoints[i]-ell+1):changePoints[i],]),2,mean)
+                    
+                    decrease=ell*num_each[i]/(num_each[i]-ell)*(mean_each[i,]-mean_candidatePart)^2 
+                    increase=ell*num_each[i+1]/(num_each[i+1]+ell)*(mean_each[i+1,]-mean_candidatePart)^2
+             
+                    if ( sum(decrease) - sum(increase) > best_gain_sum) { 
+                        #test
+                        cat("decrease=",sum(decrease),"\n")
+                        cat("increase=",sum(increase),"\n")
+                        #test
+                        best_gain = decrease - increase
+                        best_gain_sum = sum(best_gain)
+                        best_ell = ell
+                        best_candidatePart = matrix(X[(changePoints[i]-ell+1):changePoints[i],]) 
+                        #test
+                        cat("best_candidatePart=",best_candidatePart,"\n")
+                        cat("best_ell =",best_ell,"\n")
+                        #test
+                        best_decrease = decrease
+                        best_increase = increase
+                    }
+                }
+            }
+            if  (best_gain_sum > 0) {
+                #test
+                cat("left to right","\n")
+                #test
+                move = 1
+                best_mean_candidatePart = apply(best_candidatePart,2,mean)
+                mean_each[i,] = (num_each[i]*mean_each[i,]-best_ell*best_mean_candidatePart)/(num_each[i]-best_ell) 
+                mean_each[i+1,] = (num_each[i+1]*mean_each[i+1,]+best_ell*best_mean_candidatePart)/(num_each[i+1]+best_ell) 
+                changePoints[i] = changePoints[i] - best_ell
+                num_each[i] = num_each[i] - best_ell
+                num_each[i+1] = num_each[i+1] + best_ell
+                wgss_part = apply((best_candidatePart - matrix(best_mean_candidatePart,nrow=best_ell,ncol=D,byrow=TRUE))^2,2,sum)
+                wgss_each[i,] = wgss_each[i,] - best_decrease - wgss_part
+                wgss_each[i+1,] = wgss_each[i+1] + best_increase + wgss_part  
+                #test
+                cat("changePoints[i]",changePoints[i],"\n")
+                cat("num_each[i] =",num_each[i],"\n")
+                cat("num_each[i+1] =",num_each[i+1],"\n")
+                #test
+            } else {
+                best_gain_sum=-Inf
+                #test 
+                cat("num_each[i+1]=",num_each[i+1],"\n")
+                #test
+                if (num_each[i+1]>1) {
+                    for (ell in 1:(num_each[i+1]-1)) { 
+                        #test
+                        cat("ell=",ell,"\n")
+                        #test
+                        mean_candidatePart=apply(matrix(X[(changePoints[i]+1):(changePoints[i]+ell),]),2,mean)
+                        
+                        decrease=ell*num_each[i+1]/(num_each[i+1]-ell)*(mean_each[i+1,]-mean_candidatePart)^2 
+                        increase=ell*num_each[i]/(num_each[i]+ell)*(mean_each[i,]-mean_candidatePart)^2 
+                        if ( sum(decrease) - sum(increase) > best_gain_sum) { 
+                            #test
+                            cat("decrease=",sum(decrease),"\n")
+                            cat("increase=",sum(increase),"\n")
+                            #test
+                            best_gain = decrease - increase
+                            best_gain_sum = sum(best_gain)
+                            best_ell = ell
+                            best_candidatePart = matrix(X[(changePoints[i]+1):(changePoints[i]+ell),]) 
+                            #test
+                            cat("best_candidatePart=",best_candidatePart,"\n")
+                            cat("best_ell =",best_ell,"\n")
+                            #test
+                            best_decrease = decrease
+                            best_increase = increase
+                        }
+                    }
+                }
+                if  (best_gain_sum > 0) {
+                    #test
+                    cat("right to left","\n")
+                    #test
+                    move = 1
+                    best_mean_candidatePart = apply(best_candidatePart,2,mean)
+                    mean_each[i+1,] = (num_each[i+1]*mean_each[i+1,]-best_ell*best_mean_candidatePart)/(num_each[i+1]-best_ell) 
+                    mean_each[i,] = (num_each[i]*mean_each[i,]+best_ell*best_mean_candidatePart)/(num_each[i]+best_ell) 
+                    changePoints[i] = changePoints[i] + best_ell
+                    num_each[i] = num_each[i] + best_ell
+                    num_each[i+1] = num_each[i+1] - best_ell
+                    wgss_part = apply((best_candidatePart - matrix(best_mean_candidatePart,nrow=best_ell,ncol=D,byrow=TRUE))^2,2,sum)
+                    wgss_each[i,] = wgss_each[i,] + best_decrease + wgss_part
+                    wgss_each[i+1,] = wgss_each[i+1] - best_increase - wgss_part  
+                    #test
+                    cat("changePoints[i]",changePoints[i],"\n")
+                    cat("num_each[i] =",num_each[i],"\n")
+                    cat("num_each[i+1] =",num_each[i+1],"\n")
+                    #test
+                }
+            }
         }
-      }
-      if  (best_gain_sum > 0) {
-        move = 1
-        best_mean_candidatePart = apply(best_candidatePart,2,mean)
-        mean_each[i,] = (num_each[i]*mean_each[i,]-best_ell*best_mean_candidatePart)/(num_each[i]-best_ell) 
-        mean_each[i+1,] = (num_each[i+1]*mean_each[i+1,]+best_ell*best_mean_candidatePart)/(num_each[i+1]+best_ell) 
-        changePoints[i] = changePoints[i] - best_ell
-        num_each[i] = num_each[i] - best_ell
-        num_each[i+1] = num_each[i+1] + best_ell
-        wgss_part = apply((best_candidatePart - matrix(best_mean_candidatePart,nrow=best_ell,ncol=D,byrow=TRUE))^2,2,sum)
-        wgss_each[i,] = wgss_each[i,] - best_decrease - wgss_part
-        wgss_each[i+1,] = wgss_each[i+1] + best_increase + wgss_part  
-      } else {
-        best_gain_sum=-Inf
-        for (ell in 1:num_each[i+1]-1) { 
-          mean_candidatePart=apply(matrix(X[(changePoints[i]+1):changePoints[i]+ell,]),2,mean)
-          
-          decrease=ell*num_each[i+1]/(num_each[i+1]-ell)*(mean_each[i+1,]-mean_candidatePart)^2 
-          increase=ell*num_each[i]/(num_each[i]+ell)*(mean_each[i,]-mean_candidatePart)^2 
-          if ( sum(decrease) - sum(increase) > best_gain_sum) { 
-            best_gain = decrease - increase
-            best_gain_sum = sum(best_gain)
-            best_ell = ell
-            best_candidatePart = matrix(X[(changePoints[i]+1):changePoints[i]+ell,]) 
-            best_decrease = decrease
-            best_increase = increase
-          }
-        }
-        if  (best_gain_sum > 0) {
-          move = 1
-          best_mean_candidatePart = apply(best_candidatePart,2,mean)
-          mean_each[i+1,] = (num_each[i+1]*mean_each[i+1,]-best_ell*best_mean_candidatePart)/(num_each[i+1]-best_ell) 
-          mean_each[i,] = (num_each[i]*mean_each[i,]+best_ell*best_mean_candidatePart)/(num_each[i]+best_ell) 
-          changePoints[i] = changePoints[i] + best_ell
-          num_each[i] = num_each[i] + best_ell
-          num_each[i+1] = num_each[i+1] - best_ell
-          wgss_part = apply((best_candidatePart - matrix(best_mean_candidatePart,nrow=best_ell,ncol=D,byrow=TRUE))^2,2,sum)
-          wgss_each[i,] = wgss_each[i,] + best_decrease + wgss_part
-          wgss_each[i+1,] = wgss_each[i+1] - best_increase - wgss_part  
-        }
-      }
     }
     wgss=colSums(wgss_each)
-  }
 }
 wgss_sum=sum(wgss)
-
+traceback()
