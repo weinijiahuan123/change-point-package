f29cd9ecd56eca8e260cc1da3bd7b040a2daaf32
jiahuan <jiahuanye123@163.com>
2019-03-11 13:46:23 -0500

    test using simulation data

--- algorithm3.R
+++ algorithm3.R
@@ -1,1 +1,41 @@
-for 
+score=matrix(c(0,0,1,2,0,1,2,2,1,2,3,4,1,2,3,3,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,2,0,1,2,3,0,1,2,3,0,0,0,0,0,0,0,0),ncol=4,byrow=TRUE)
+tolerance=2
+
+N=dim(score)[1];R=dim(score)[2]
+S=max(score[,-1])
+M_max=3
+
+for (r in R:1) {
+    J=list()
+    num=0
+    #test
+    #r=4
+    #test
+    if (score[1,r]>=S-tolerance) {
+        num=num+1
+        J[[num]]=1
+    }
+    for (i in 2:N) {
+        if (score[i,r]>=S-tolerance) {
+            if (score[i,r]!=score[i-1,r]) {
+                num=num+1
+                J[[num]]=i
+            } else {
+                J[[num]]=c(J[[num]],i)
+            }
+        }
+ 
+    }
+    if (num<=M_max) {
+        break
+    }
+}
+#output
+M_optimal=num
+cp=J
+r_optimal=r
+#x_optimal=list()
+#for (i in 1:num) {
+#    print(score[J[[i]],r])
+#}
+
