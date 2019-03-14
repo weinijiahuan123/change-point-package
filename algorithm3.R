52bb690b3af0be275b7dd05c6b7529a4c7398eec
jiahuan <jiahuanye123@163.com>
2019-03-14 04:53:15 -0500

    change to function format

--- algorithm3.R
+++ algorithm3.R
@@ -4,31 +4,36 @@ tolerance=2
 N=dim(score)[1];R=dim(score)[2]
 S=max(score[,-1])
 M_max=3
-
-for (r in R:1) {
-    J=list()
-    num=0
-    #test
-    #r=4
-    #test
-    if (score[1,r]>=S-tolerance) {
-        num=num+1
-        J[[num]]=1
-    }
-    for (i in 2:N) {
-        if (score[i,r]>=S-tolerance) {
-            if (score[i,r]!=score[i-1,r]) {
-                num=num+1
-                J[[num]]=i
-            } else {
-                J[[num]]=c(J[[num]],i)
-            }
+PeakRange=function(score,tolerance=1,M_max=4) {
+    N=dim(score)[1];R=dim(score)[2]
+    S=max(score[,-1])
+    for (r in R:1) {
+        J=list()
+        num=0
+        #test
+        #r=4
+        #test
+        if (score[1,r]>=S-tolerance) {
+            num=num+1
+            J[[num]]=1
         }
- 
+        for (i in 2:N) {
+            if (score[i,r]>=S-tolerance) {
+                if (score[i,r]!=score[i-1,r]) {
+                    num=num+1
+                    J[[num]]=i
+                } else {
+                    J[[num]]=c(J[[num]],i)
+                }
+            }
+     
+        }
+        if (num<=M_max) {
+            break
+        }
     }
-    if (num<=M_max) {
-        break
-    }
+    optimal=list(best_nRange=num,best_Ranges=J,best_nWindowType=r)
+    return(optimal)
 }
 #output
 M_optimal=num
