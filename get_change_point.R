6ba19b793f4d743270f5e97d3bbfaa63c3dd20db
jiahuan <jiahuanye123@163.com>
2019-05-01 04:23:52 -0500

    use log likelihood to compare points

--- get_change_point.R
+++ get_change_point.R
@@ -1,55 +1,86 @@
-RangeToPoint=function(x,n,location) {
-    # Transform the peak ranges of change points to exact change points.
-    #
-    # Args:
-    #   x: The original data to find change points. Must be one dimensional data
-    #   n: The number of peak ranges of change points
-    #   location: The location of ranges of change points
-    #
-    # Returns:
-    #   change_point: The location of exact change points
-    len=length(x)
-    change_point=numeric(n+1)
-    # add 1 as the first change point for computation convenience
-    change_point[1]=1
-    for (i in 1:n) {
-        best_loss=Inf
-        #test
-        #i=1
-        #test
-        #cat("i=",i,"\n")
-        # Get the outer range to find the exact change point.
-        if (n==1) {
-            outer_range=x
-        } else {
-            if (i == n) {
-                outer_range=x[change_point[i]:len]
-            } else {
-            outer_range=x[change_point[i]:(location[[i+1]][1]-1)]
-            }
-        }
-        # Fix following ranges and find the exact change point of range i with smallest quadratic loss.
-        for (e in 1:length(location[[i]])) {
-            #test
-            #e=1
-            #test
-            loss=var(outer_range[1:location[[i]][e]])*(location[[i]][e]-1)+var(outer_range[location[[i]][e]+1]:length(outer_range))*(length(outer_range)-location[[i]][e]-1)
-            if (loss<best_loss) {
-                best_loss=loss
-                change_point[i+1]=location[[i]][e]
-            }
-            #cat("e",e,"\n")
-            #cat("loss",loss,"\n")
-            #cat("best_loss",best_loss,"\n")
-            #cat("change_point",change_point,"\n")
-        }
+RangeToPoint<-function(y,n_peak_range,peak_range,get_loglik=EstimateAr) {
+  # Transform the peak ranges of change points to exact change points.
+  #
+  # Args:
+  #   y: The original data to find change points. Must be one dimensional data
+  #   n_peak_range: The number of peak ranges of change points
+  #   peak_range: The location of ranges of change points
+  #
+  # Returns:
+  #   change_point: The location of exact change points
+  len <- length(y)
+  # Initialize change points
+  change_point <- numeric(n_peak_range)
+  for (i in 1:n_peak_range) {
+    change_point[i] <- ceiling(mean(peak_range[[i]]))
+  }
+  # add 1 as the first change point and N as the last change point for computation convenience
+  change_point <- c(1, change_point, N)
+
+  for (i in 1:n_peak_range) {
+    best_log_lik <- Inf
+    #test
+    #i=1
+    #test
+    #cat("i=",i,"\n_peak_range")
+    # Get the outer range to find the exact change point.
+
+#
+#     if (i == n_peak_range) {
+#       outer_range<-y[change_point[i]:len]
+#     } else {
+#       outer_range<-y[change_point[i]:(peak_range[[i+1]][1]-1)]
+#     }
+
+    # Fix following ranges and find the exact change point of range i with smallest quadratic loss.
+    for (e in 1:length(peak_range[[i]])) {
+      #test
+      #e=1
+      #test
+      left_part <- (peak_range[[i]][e]-change_point[i])*log(get_loglik(y,change_point[i]+1,peak_range[[i]][e],2)$sigma2)
+      right_part <- (change_point[i+2]-peak_range[[i]][e])*log(get_loglik(y,peak_range[[i]][e]+1,change_point[i+2],2)$sigma2)
+      log_lik <- left_part+right_part
+
+      if (log_lik < best_log_lik) {
+        best_log_lik <- log_lik
+        change_point[i+1] <- peak_range[[i]][e]
+      }
+      #cat("e",e,"\n")
+      #cat("loss",loss,"\n")
+      #cat("best_log_lik",best_log_lik,"\n")
+      #cat("change_point",change_point,"\n")
     }
-    # Delete the first change point wich is fixed as 1
-    change_point=change_point[-1]
-    return(change_point)
+  }
+  # Delete the first and last change points
+  change_point <- change_point[-c(1,length(change_point))]
+  return(change_point)
 
 }
-RangeToPoint(x,n,location)
-n=peakrange$n_peak_range
-location=peakrange$peak_ranges
+a=seq(70,105)
+b=seq(395,420)
+peak_range<-list(a,b)
+RangeToPoint(y,n_peak_range,peak_range)
+res<- MultiWindow(y,window_list=c(100,50,20,10,5),point_max=5,seg_min=2,tolerance=1)
+n_peak_range=res$n_peak_range
+peak_range=res$peak_range
 
+EstimateAr=function(x,T1,T2,L){
+  if (T1>(T2-L)) {
+    warning("Error in estimate_ar")
+  }
+  if (T1<=L) {
+    T1=L+1
+  }
+  Y=matrix(0,nrow=L+1,ncol=T2-T1+1)
+  Y[1,]=1
+  for (k in 1:L) {
+    Y[k+1,]=x[(T1-k):(T2-k)]
+  }
+  A=Y%*%t(Y)
+  B=Y%*%x[T1:T2]
+  C=solve(A)%*%B
+  e=x[T1:T2]-t(Y)%*%C
+  sigma2=sum(e^2)/(T2-T1+1)
+  est_coef=list(C=C,sigma2=sigma2)
+  return(est_coef)
+}
